b4aa275b4d88f26d32ba1ed3d1edb75971964ecf
Michel van der Hulst <michel@voorkanter.com>
2018-02-25 21:31:37 +0100

    Add noise to water to create natural looking waves

--- src/Main.elm
+++ src/Main.elm
@@ -32,8 +32,8 @@ view t =
         , style [ ( "display", "block" ) ]
         ]
         [ WebGL.entity
-            vertexShader
-            fragmentShader
+            waterVertexShader
+            waterFragmentShader
             mesh
             { perspective = perspective
             , camera = camera
@@ -49,7 +49,7 @@ perspective =
 
 camera : Mat4
 camera =
-    Mat4.makeLookAt (vec3 15 50 75) (vec3 15 0 0) (vec3 0 1 0)
+    Mat4.makeLookAt (vec3 60 150 250) (vec3 60 0 0) (vec3 0 1 0)
 
 
 
@@ -65,7 +65,6 @@ type alias Vertex =
     , vert1 : Vec2
     , vert2 : Vec2
     , vert3 : Vec2
-    , color : Vec3
     }
 
 
@@ -73,7 +72,7 @@ mesh : Mesh Vertex
 mesh =
     let
         size =
-            16
+            64
 
         list =
             List.range 0 size
@@ -115,19 +114,15 @@ square x z =
         sw =
             (vec2 (1 + x * 2) (1 + z * 2))
     in
-        [ ( Vertex nw nw so sw (vec3 1 0 0)
-          , Vertex so nw so sw (vec3 0 1 0)
-          , Vertex sw nw so sw (vec3 0 0 1)
+        [ ( Vertex nw nw so sw
+          , Vertex so nw so sw
+          , Vertex sw nw so sw
           )
-        , ( Vertex sw sw no nw (vec3 1 0 0)
-          , Vertex no sw no nw (vec3 0 1 0)
-          , Vertex nw sw no nw (vec3 0 0 1)
+        , ( Vertex sw sw no nw
+          , Vertex no sw no nw
+          , Vertex nw sw no nw
           )
         ]
-
-
-
--- Shaders
 
 
 type alias Uniforms =
@@ -137,33 +132,55 @@ type alias Uniforms =
     }
 
 
-
-
-
-vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
-vertexShader =
+waterVertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
+waterVertexShader =
     [glsl|
         attribute vec2 position;
         attribute vec2 vert1;
         attribute vec2 vert2;
         attribute vec2 vert3;
-        attribute vec3 color;
         uniform mat4 perspective;
         uniform mat4 camera;
         uniform float t;
         varying vec3 vcolor;
 
+        vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+
+        float snoise(vec2 v){
+          const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+                   -0.577350269189626, 0.024390243902439);
+          vec2 i  = floor(v + dot(v, C.yy) );
+          vec2 x0 = v -   i + dot(i, C.xx);
+          vec2 i1;
+          i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+          vec4 x12 = x0.xyxy + C.xxzz;
+          x12.xy -= i1;
+          i = mod(i, 289.0);
+          vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+          + i.x + vec3(0.0, i1.x, 1.0 ));
+          vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),
+            dot(x12.zw,x12.zw)), 0.0);
+          m = m*m ;
+          m = m*m ;
+          vec3 x = 2.0 * fract(p * C.www) - 1.0;
+          vec3 h = abs(x) - 0.5;
+          vec3 ox = floor(x + 0.5);
+          vec3 a0 = x - ox;
+          m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+          vec3 g;
+          g.x  = a0.x  * x0.x  + h.x  * x0.y;
+          g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+          return 130.0 * dot(m, g);
+        }
+
         float calculateSurface(float x, float z) {
-            float scale = 20.0;
-            float y = 0.0;
+            float scale = 40.0;
+            float y = snoise(vec2(x + t / 3.0,z + t / 3.0)) / 2.0;
             y += (sin(x * 1.0 / scale + t * 1.0) + sin(x * 2.3 / scale + t * 1.5) + sin(x * 3.3 / scale + t * 0.4)) / 3.0;
             y += (sin(z * 0.2 / scale + t * 1.8) + sin(z * 1.8 / scale + t * 1.8) + sin(z * 2.8 / scale + t * 0.8)) / 3.0;
-            y += cos(x * 1.25 - t) * sin(z * 0.75 + t) * 0.5;
 
             return y;
         }
-
-
 
         void main () {
             float y = calculateSurface(position.x, position.y);
@@ -179,8 +196,8 @@ vertexShader =
     |]
 
 
-fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
-fragmentShader =
+waterFragmentShader : Shader {} Uniforms { vcolor : Vec3 }
+waterFragmentShader =
     [glsl|
         precision mediump float;
 
