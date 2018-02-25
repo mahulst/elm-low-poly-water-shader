b1a3eca52b212cfa3bd15d78c8f617ecc40a9a7f
Michel van der Hulst <michel@voorkanter.com>
2018-02-25 21:19:16 +0100

    Stationary first triangle

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ import AnimationFrame
 import Html exposing (Html)
 import Html.Attributes exposing (width, height, style)
 import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector2 exposing (Vec2, vec2)
 import Math.Vector3 as Vec3 exposing (vec3, Vec3)
 import Time exposing (Time)
 import WebGL exposing (Mesh, Shader)
@@ -34,15 +35,21 @@ view t =
             vertexShader
             fragmentShader
             mesh
-            { perspective = perspective (t / 1000) }
+            { perspective = perspective
+            , camera = camera
+            , t = t
+            }
         ]
 
 
-perspective : Float -> Mat4
-perspective t =
-    Mat4.mul
-        (Mat4.makePerspective 45 1 0.01 100)
-        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
+perspective : Mat4
+perspective =
+    Mat4.makePerspective 45 1 0.01 1000
+
+
+camera : Mat4
+camera =
+    Mat4.makeLookAt (vec3 0 25 25) (vec3 0 0 0) (vec3 0 1 0)
 
 
 
@@ -58,9 +65,9 @@ type alias Vertex =
 mesh : Mesh Vertex
 mesh =
     WebGL.triangles
-        [ ( Vertex (vec3 0 0 0) (vec3 1 0 0)
-          , Vertex (vec3 1 1 0) (vec3 0 1 0)
-          , Vertex (vec3 1 -1 0) (vec3 0 0 1)
+        [ ( Vertex (vec3 1 0 0) (vec3 1 0 0)
+          , Vertex (vec3 0 0 -1) (vec3 0 1 0)
+          , Vertex (vec3 0 0 1) (vec3 0 0 1)
           )
         ]
 
@@ -70,7 +77,13 @@ mesh =
 
 
 type alias Uniforms =
-    { perspective : Mat4 }
+    { perspective : Mat4
+    , camera : Mat4
+    , t : Float
+    }
+
+
+
 
 
 vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
@@ -80,25 +93,26 @@ vertexShader =
         attribute vec3 position;
         attribute vec3 color;
         uniform mat4 perspective;
+        uniform mat4 camera;
         varying vec3 vcolor;
 
         void main () {
-            gl_Position = perspective * vec4(position, 1.0);
+            gl_Position = perspective * camera * vec4(position, 1.0);
             vcolor = color;
         }
-
     |]
 
 
 fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
 fragmentShader =
     [glsl|
-
         precision mediump float;
+
         varying vec3 vcolor;
 
-        void main () {
-            gl_FragColor = vec4(vcolor, 1.0);
+        void main() {
+          vec3 terrainColor = vcolor;
+          gl_FragColor = vec4(terrainColor, 1.0);
         }
 
     |]
